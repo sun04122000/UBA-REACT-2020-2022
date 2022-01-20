f8bd82dfc82fda401b99161a5667b5b9b44640f2
sun04122000 <sunidhi.jindal001@gmail.com>
2022-01-21 01:50:06 +0530

    shared the project folder structure

---
+++ src/screens/Home/Homescreen/Header.js
@@ -0,0 +1,200 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Navbar,
+  Nav,
+  Container,
+  NavDropdown,
+  Row,
+  Image,
+  Button,
+  Carousel,
+} from "react-bootstrap";
+import logo from "./home/images/logo.png";
+import slider1 from "./home/images/header_images/1.png";
+import slider2 from "./home/images/header_images/2.png";
+import slider3 from "./home/images/header_images/3.jpeg";
+import slider4 from "./home/images/header_images/4.jpg";
+import slider5 from "./home/images/header_images/5.jpg";
+import slider6 from "./home/images/header_images/6.jpg";
+import slider7 from "./home/images/header_images/7.jpeg";
+import slider8 from "./home/images/header_images/8.jpeg";
+import slider9 from "./home/images/header_images/9.jpeg";
+import slider10 from "./home/images/header_images/10.jpeg";
+import slider11 from "./home/images/header_images/11.jpeg";
+import slider12 from "./home/images/header_images/12.jpg";
+import "./css/Header.css";
+
+export const Header = () => {
+  return (
+    <>
+      {/* Navbar part starts here */}
+      <header>
+        <Container fluid>
+          <Row
+            className="header-part"
+            style={{ background: "#03506f", height: "1.3rem" }}
+          ></Row>
+          <Row>
+            <Image src={logo} alt="logo.png" fluid />
+          </Row>
+        </Container>
+        <Navbar
+          className="navBar"
+          style={{ background: "#fbe58a" }}
+          expand="lg"
+        >
+          <Container className="navContainer">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link className="mx-3" href="#home">
+                  Home
+                </Nav.Link>
+                <NavDropdown
+                  className="mx-3"
+                  title="Initiatives"
+                  id="basic-nav-dropdown"
+                  renderMenuOnMount={true}
+                >
+                  <NavDropdown.Item href="#action/3.1">
+                    Education
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.2">Health</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.3">
+                    Technology
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Awareness
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.5">Other</NavDropdown.Item>
+                </NavDropdown>
+
+                <NavDropdown
+                  className="mx-3"
+                  title="Villages"
+                  id="basic-nav-dropdown"
+                  renderMenuOnMount={true}
+                >
+                  <NavDropdown.Item href="#action/3.1">Beladi</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.2">
+                    Chharba
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.3">
+                    Chandpur
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Meerpur
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.5">
+                    Puranpur
+                  </NavDropdown.Item>
+                </NavDropdown>
+
+                <Nav.Link className="mx-3" href="#events">
+                  Events
+                </Nav.Link>
+
+                <NavDropdown
+                  className="mx-3"
+                  title="Teams"
+                  id="basic-nav-dropdown"
+                  renderMenuOnMount={true}
+                >
+                  <NavDropdown.Item href="#action/3.1">
+                    Faculty
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.2">
+                    Students
+                  </NavDropdown.Item>
+                </NavDropdown>
+
+                <Nav.Link className="mx-3" href="#gallery">
+                  Gallery
+                </Nav.Link>
+
+                <Nav.Link className="mx-3" href="#contact">
+                  Contact
+                </Nav.Link>
+
+                <Nav.Link className="mx-1" href="###">
+                  <Button
+                    className="subscribeBtn"
+                    id="subsc1"
+                    variant="secondary"
+                    size="sm"
+                    type="button"
+                    data-bs-toggle="modal"
+                    data-bs-target="#myModal"
+                    
+                  >
+                    <i className="fa fa-envelope mx-2"></i>
+                    SUBSCRIBE TO OUR NEWSLETTER
+                  </Button>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+          <Button
+            className="subscribeBtn"
+            id="subsc2"
+            variant="secondary"
+            size="sm"
+          >
+            <i className="fa fa-envelope mx-2"></i>
+            SUBSCRIBE TO OUR NEWSLETTER
+          </Button>
+        </Navbar>
+      </header>
+
+      {/* Head Carousel starts here */}
+      <Carousel interval={null} fade>
+        <Carousel.Item>
+          <img className="d-block w-100" src={slider1} alt="First slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={slider2} alt="Second slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={slider3} alt="Third slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={slider4} alt="Fourth slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={slider5} alt="Fifth slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={slider6} alt="Sixth slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={slider7} alt="Seventh slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={slider8} alt="Eighth slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={slider9} alt="Ninth slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={slider10} alt="Tenth slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={slider11} alt="Eleventh slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={slider12} alt="Twelfth slide" />
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
+};
---
+++ src/screens/Home/Homescreen/HomeScreen.js
@@ -0,0 +1,117 @@
+// import React, { useState, useEffect, useRef } from 'react'
+// import Set from './home/Set'
+import IdentityCard from './home/IdentityCard'
+import { InitCard } from './home/InitCard'
+import { Images_sequence } from './home/Images_sequence'
+import food_kit from './home/images/Beladi/CycleDonation/covid.jpg'
+import cycle_donation from './home/images/Beladi/CycleDonation/i.jpg'
+import swacchta_hi_seva from './home/images/Chharba/PlasticFreeIndia_Chharba/i.jpeg'
+import director from './home/images/patron/director.JPG'
+import mparida from './home/images/patron/mprdaf.jpg'
+import ap from './home/images/patron/Ap.jpeg'
+import bhanu from './home/images/patron/bhanu.jpg'
+import Inspiration from './home/Inspiration'
+import { BsFillHeartFill } from 'react-icons/bs';
+// import "./home/css/Style.css"
+import { Work } from './home/Work'
+import "./css/App.css"
+import About from './home/About'
+import './css/Carousel.css';
+import ImageSlider from "./home/Slider";
+// import "./home/css/InitCard.css"
+
+
+export const HomeScreen = () => {
+
+
+  return (
+    <>
+
+
+      <About />
+
+
+      <div className="container">
+        <section >
+          {/* <!--======= TITTLE =========--> */}
+          <div className="tittle">
+            <h2>Our Recent <span>Initiatives </span></h2>
+            <p>BrighInitCard <span className="underline">minds at work</span> here</p>
+          </div>
+          <div className="row">
+            <div className="col-lg-4">
+              <center>
+                <InitCard image={food_kit} village="All Adopted villages |" date="April 07, 2020" init="Food kit distribution" initiative="COVID19: Team UBA IIT Roorkee distributed food kits" /><br/>
+              </center>
+            </div>
+            <div className="col-lg-4">
+              <center>
+                <InitCard image={cycle_donation} village="Beladi |" date="October 12, 2019" init="Cycle Donation" initiative="Donating unused cycles at IITR to village students in Beladi." /><br/>
+              </center>
+            </div>
+            <div className="col-lg-4">
+              <center>
+                <InitCard image={swacchta_hi_seva} village="Chharba |" date="September 29, 2019" init="Swacchta Hi Seva" initiative="A step towards greener and cleaner India promoted through a campaign.." /><br/>
+              </center>
+            </div>
+          </div>
+        </section>
+      </div>
+      <Work />
+      <Images_sequence /><br/><br/>
+      <h2><center>Our <b>Patrons</b></center></h2>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-1">
+          </div>
+          <div className="col-md-5">
+            <IdentityCard name="Prof. Ajit K. Chaturvedi"
+              designation="Director, IIT Roorkee"
+              image={director}
+              link="https://www.iitr.ac.in/institute/pages/Director_IIT_Roorkee.html" />
+          </div>
+          <div className="col-md-5">
+            <IdentityCard name="Prof. Manoranjan Parida"
+              designation="Dy Director, IIT Roorkee"
+              image={mparida}
+              link="https://www.iitr.ac.in/departments/CE/pages/People+Faculty+Parida_M_.html" />
+          </div>
+          <div className="col-md-1"></div>
+        </div>
+        <div className="row">
+          <div className="col-md-1"></div>
+          <div className="col-md-5">
+            <IdentityCard name="Dr.Bhanu Prakash Vellanki"
+              designation="Co-ordinator, UBA IIT Roorkee"
+              image={bhanu}
+              link="https://civil.iitr.ac.in/CE?Uid=bhanufce" />
+          </div>
+          <div className="col-md-5">
+            <IdentityCard name="Prof. Ashish Pandey"
+              designation="Regional Cordinator, UBA"
+              image={ap}
+              link="https://www.iitr.ac.in/departments/WRT/pages/People+Faculty+ashisfwt.html" />
+          </div>
+          <div className="col-md-1"></div>
+        </div>
+      </div>
+      <div className="container">
+        {/* <!--======= TITTLE =========--> */}
+        <div className="tittle">
+          <h2>Fresh From The <span>Blog</span></h2>
+          <p>Work.  Innovation.  Fun.</p>
+        </div>
+        <div className="row mt-5 carousel-blog carousel">
+
+          <ImageSlider />
+        </div>
+      </div>
+      <Inspiration />
+      {/* <!--======= PROUD =========--> */}
+      <section ><br/>
+        <center><h2>Be the change <span className="Heart"><BsFillHeartFill /></span> <strong>You wish to see</strong>.</h2></center>
+      </section>
+    </>
+  )
+}
+
