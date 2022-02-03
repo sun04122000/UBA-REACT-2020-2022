ba2c40f2f4f9e173b42f7e60e1221d5dac09f13a
Dev <devsri001@gmail.com>
2022-02-03 23:27:35 +0530

    Homescreen-commit_Events-page-commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { HomeScreen } from "./screens/HomeScreen/HomeScreen";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import {InitCard_Events} from "./screens/Events/InitCard_Events"
+import "./App.css"
 
 function App() {
   return (
+    <>
    <HomeScreen/>
+    {/* <InitCard_Events/> */}
+    </>
   );
 }
 
