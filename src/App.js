dd3ce08cbcb17f0b73e4b0c5d81d9070f3471839
Aman Yadav <aman.ydv.2112@gmail.com>
2022-02-08 02:39:51 +0530

    Added Navbar_StudentTeam_VillagesPage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import { HomeScreen } from "./screens/HomeScreen/HomeScreen";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Header } from "./screens/HomeScreen/Javascript/Header.js";
+import { StudentTeam } from "./screens/HomeScreen/Javascript/StudentTeam";
+import { Beladi } from "./screens/HomeScreen/Javascript/Villages/Beladi";
+import { Chandpur } from "./screens/HomeScreen/Javascript/Villages/Chandpur";
+import { Chharba } from "./screens/HomeScreen/Javascript/Villages/Chharba";
+import { Meerpur } from "./screens/HomeScreen/Javascript/Villages/Meerpur.js";
+import { Puranpur } from "./screens/HomeScreen/Javascript/Villages/Puranpur.js";
 
 function App() {
   return (
-   <HomeScreen/>
+    <Router>
+      <HomeScreen />
+    </Router>
   );
 }
 
